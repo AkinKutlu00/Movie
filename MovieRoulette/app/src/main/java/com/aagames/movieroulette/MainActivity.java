d9718fa37595153d5fd7404c45057d1272f640a0
Akın Kutlu <akin.kutlu@ug.bilkent.edu.tr>
2020-08-26 19:27:35 +0300

    eklemeler

--- MovieRoulette/app/src/main/java/com/aagames/movieroulette/MainActivity.java
+++ MovieRoulette/app/src/main/java/com/aagames/movieroulette/MainActivity.java
@@ -12,7 +12,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -23,6 +26,7 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
@@ -35,22 +39,29 @@ public class MainActivity extends AppCompatActivity {
     private Button random;
     Button plus;
     Button minus;
+    Spinner spinner;
+    List< String > filter;
+    TextView title;
 
     //silinecek
     Button button;
 
     int maxNumber;
+    int spinnerNo;
+    int mod;
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mod=0;
 
         //silinecek
         final Intent intent = new Intent(this, Login.class);
 
-        button= (Button) findViewById(R.id.button9);
+        button = (Button) findViewById(R.id.button9);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -59,10 +70,15 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+       //silin sonu
 
-        //silin sonu
+
+        title = (TextView) findViewById(R.id.title);
 
 
+        title.setText("Mubi Top 100");
+
+        spinner = findViewById(R.id.spinner);
 
         final ArrayList<MovieItem> movieList1 = new ArrayList<>();
         final ArrayList<MovieItem> movieListBlue = new ArrayList<>();
@@ -74,6 +90,25 @@ public class MainActivity extends AppCompatActivity {
         //myRef.child("0").child("revealed").setValue(true);
         //movieList1.get(0).setRevealed(true);
 
+        filter = new ArrayList<>();
+
+        filter.add("Imdb");
+        filter.add("Rotten Tomatoes");
+        filter.add("Mubi");
+        filter.add("Comedy");
+        filter.add("Science Fiction");
+        filter.add("Documentary");
+        filter.add("New Category");
+
+
+        TextView tv= (TextView) findViewById(R.id.title);
+
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>( this, android.R.layout.simple_spinner_item, filter );
+        adapter.setDropDownViewResource( android.R.layout.simple_spinner_dropdown_item );
+
+        spinner.setAdapter( adapter );
+        spinner.setSelection(0);
 
 
         myRecyclerView = findViewById(R.id.myRecyclerView);
@@ -84,23 +119,70 @@ public class MainActivity extends AppCompatActivity {
         minus = (Button) findViewById(R.id.minus);
 
 
+
+
         plus.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                mLayoutManager = new GridLayoutManager(getApplicationContext(),3);
-                myRecyclerView.setLayoutManager(mLayoutManager);
-                mAdapter = new MovieAdapterPlus(getApplicationContext(),movieList1);
-                myRecyclerView.setAdapter(mAdapter);
+                if(mod < 2){
+                    mod++;;
+
+                }
+
+                if(mod == 0){
+                    mLayoutManager = new GridLayoutManager(getApplicationContext(),10);
+                    myRecyclerView.setLayoutManager(mLayoutManager);
+                    mAdapter = new MovieAdapter(getApplicationContext(),movieList1);
+                    myRecyclerView.setAdapter(mAdapter);
+
+
+                }else if(mod == 1){
+                    mLayoutManager = new GridLayoutManager(getApplicationContext(),3);
+                    myRecyclerView.setLayoutManager(mLayoutManager);
+                    mAdapter = new MovieAdapterPlus(getApplicationContext(),movieList1);
+                    myRecyclerView.setAdapter(mAdapter);
+
+
+                }else{
+                    mLayoutManager = new GridLayoutManager(getApplicationContext(),1);
+                    myRecyclerView.setLayoutManager(mLayoutManager);
+                    mAdapter = new MovieAdapterPlus(getApplicationContext(),movieList1);
+                    myRecyclerView.setAdapter(mAdapter);
+
+                }
+
             }
         });
 
         minus.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                mLayoutManager = new GridLayoutManager(getApplicationContext(),10);
-                myRecyclerView.setLayoutManager(mLayoutManager);
-                mAdapter = new MovieAdapter(getApplicationContext(),movieList1);
-                myRecyclerView.setAdapter(mAdapter);
+                if(mod > 0){
+                    mod--;
+
+                }
+
+                if(mod == 0){
+                    mLayoutManager = new GridLayoutManager(getApplicationContext(),10);
+                    myRecyclerView.setLayoutManager(mLayoutManager);
+                    mAdapter = new MovieAdapter(getApplicationContext(),movieList1);
+                    myRecyclerView.setAdapter(mAdapter);
+
+
+                }else if(mod == 1){
+                    mLayoutManager = new GridLayoutManager(getApplicationContext(),3);
+                    myRecyclerView.setLayoutManager(mLayoutManager);
+                    mAdapter = new MovieAdapterPlus(getApplicationContext(),movieList1);
+                    myRecyclerView.setAdapter(mAdapter);
+
+
+                }else{
+                    mLayoutManager = new GridLayoutManager(getApplicationContext(),1);
+                    myRecyclerView.setLayoutManager(mLayoutManager);
+                    mAdapter = new MovieAdapterPlus(getApplicationContext(),movieList1);
+                    myRecyclerView.setAdapter(mAdapter);
+
+                }
             }
         });
 
@@ -113,14 +195,11 @@ public class MainActivity extends AppCompatActivity {
                 maxNumber = (int) snapshot.getChildrenCount();
                 for( DataSnapshot dataSnapshot1: snapshot.getChildren() )
                 {
-
-
                     MovieItem n = dataSnapshot1.getValue( MovieItem.class );
                     //System.out.println(n.getName()+" hakan ");
                     movieList1.add( n );
                     if(n.getRevealed() == false){
                         movieListBlue.add(n);
-
 
                     }
 
@@ -150,10 +229,33 @@ public class MainActivity extends AppCompatActivity {
 
         System.out.println("maxnumber: "+  maxNumber);
 
+        spinner.setOnItemSelectedListener( new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected( AdapterView<?> parent, View view, int position, long id ) {
+
+                title.setText(spinner.getSelectedItem().toString()+" (30" +"/"+"100)");
+
+                if(position == 6){
+
+
+                    startActivity(new Intent(getApplicationContext(),NewCategory.class));
+                }
+
+
+            }
+
+            @Override
+            public void onNothingSelected( AdapterView<?> parent ) {
+
+            }
+        });
+    }
 
 
 
     }
 
 
-}
+
+
+
---
+++ MovieRoulette/app/src/main/java/com/aagames/movieroulette/NewCategory.java
@@ -0,0 +1,14 @@
+package com.aagames.movieroulette;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NewCategory extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_category);
+    }
+}
