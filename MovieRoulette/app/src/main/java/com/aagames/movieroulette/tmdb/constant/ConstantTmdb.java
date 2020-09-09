d51381fc246a866427c43e0f4170b247cd15a348
Akın Kutlu <akin.kutlu@ug.bilkent.edu.tr>
2020-09-10 00:30:21 +0300

    Revert "tmdb java"
    This reverts commit 14954745f85d86761bcbee8fcf6c42c1ecb91b97.

--- MovieRoulette/app/src/main/java/com/aagames/movieroulette/tmdb/constant/ConstantTmdb.java
+++
@@ -1,8 +0,0 @@
-package com.aagames.movieroulette.tmdb.constant;
-
-import com.aagames.movieroulette.BuildConfig;
-
-public class ConstantTmdb {
-    public static final String APIKEY = BuildConfig.API_KEY;
-    public static final String BASE_URL = "https://api.themoviedb.org/3/";
-}
